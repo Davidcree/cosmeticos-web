b3ed73166263c00d869ac71b3a6ded5d1dfac645
David Caballero <dav.97,.cr@gmail.com>
2026-04-26 14:31:24 -0500

    agrego imagenes y menu lateral

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 # 🔹 Datos simulados (luego esto se cambia por SQL)
 def get_productos():
     return [
-        {"id": 1, "nombre": "Labial Mate", "precio": 25},
-        {"id": 2, "nombre": "Base Líquida", "precio": 40},
-        {"id": 3, "nombre": "Perfume Floral", "precio": 80},
-        {"id": 4, "nombre": "Sombra", "precio": 500}
+        {"id": 1, "nombre": "Labial Mate", "precio": 25, "imagen": "/static/img/labial.jpg"},
+        {"id": 2, "nombre": "Base Líquida", "precio": 40, "imagen": "/static/img/base.jpg"},
+        {"id": 3, "nombre": "Perfume Floral", "precio": 80, "imagen": "/static/img/perfume.jpg"},
+        {"id": 4, "nombre": "Sombra", "precio": 500, "imagen": "/static/img/sombra.jpg"}
     ]
 
 @app.route("/")
